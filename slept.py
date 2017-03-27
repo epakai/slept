fc59d7f726392c22d6357772773eb4af59df2719
Joshua Honeycutt <joshua.honeycutt@gmail.com>
2017-03-27 01:31:17 -0500

    change date option to a normal argument (-d/--date) time specification is now formatted 'starttime-endtime' Added argument parsing Added validation and conversion for date argument

--- slept.py
+++ slept.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-import sqlite3
+from pathlib import Path
 import getpass
 import os
-from pathlib import Path
+import sqlite3
+import argparse
+import sys
+from datetime import date, datetime
+
+def display_log():
+    print("RUNNING DISPLAY LOG")
+    return
 
 try:
     config_file = Path( os.environ["XDG_CONFIG_HOME"] + "slept.conf" )
@@ -22,12 +29,50 @@ db = conn.cursor()
 
 db.execute('''CREATE TABLE IF NOT EXISTS times
         (date TEXT, start_time TEXT, end_time TEXT)''')
-
-db.execute('''INSERT INTO times VALUES
-        (date('now'), time('12:30'), time('04:00'))''')
-db.execute('''INSERT INTO times VALUES
-        (date('now'), time('05:00'), time('07:00'))''')
 conn.commit()
 
-db.execute('''SELECT * FROM times WHERE date = date('now')''')
-print(db.fetchall())
+parser = argparse.ArgumentParser(description='Slept - a sleep logger.')
+parser.add_argument('-a', '--amend', dest='amend', action='store_true',
+        help ='amend a previous date\'s entry')
+parser.add_argument('-d', '--date', dest='date', type=str, nargs=1,
+        help ='specify a prior date')
+parser.add_argument('timespec', type=str, nargs='*',
+        help ='start and end time in 24-hour format separated by \'-\' ')
+args = parser.parse_args()
+print("### ARGS ###")
+print(args.amend)
+print(args.date)
+print(args.timespec)
+print("### ARGS ###")
+
+if (not len(sys.argv) > 1):
+    display_log()
+    sys.exit()
+if (not args.date == None):
+    try:
+        sleep_date = datetime.strptime(args.date[0], '%Y-%m-%d')
+    except ValueError:
+        try: 
+            sleep_date = datetime.strptime(args.date[0], '%m-%d')
+            sleep_date = sleep_date.replace(year=date.today().year)
+        except ValueError:
+            print('Invalid date argument')
+            sys.exit()
+    print(sleep_date)
+# else
+    # date is 'now'
+# if date exists in db and -a not set
+    # error mentioning -a option
+# else if date exists and args.amend = True
+    # delete all items with that date and continue
+# check all time pairs for validity and non-overlapping (times are 24-hour
+
+
+################################ sample queries
+#db.execute('''INSERT INTO times VALUES
+#        (date(''), time(''), time(''))''')
+#conn.commit()
+
+#db.execute('''SELECT * FROM times WHERE date = date('')''')
+#print(db.fetchall())
+
