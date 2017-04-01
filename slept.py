95289d85e8e01e2c3ae399d657d9f7f2df5b9685
Joshua Honeycutt <joshua.honeycutt@gmail.com>
2017-04-01 00:35:13 -0500

    Change datetime import and calls

--- slept.py
+++ slept.py
@@ -5,28 +5,28 @@ import os
 import sqlite3
 import argparse
 import sys
-from datetime import date, datetime, timedelta, time
-import math
+import datetime
 import curses
+import math
 
 # array of times to be highlighted
-ideal_time = [(time(22, 0), time(6, 0))]
+ideal_time = [(datetime.time(22, 0), datetime.time(6, 0))]
 
 
 def datespec_conv(datespec):
     # convert date argument to a datetime object
     try:
-        sleep_date = datetime.strptime(datespec[0], '%Y-%m-%d').date()
+        sleep_date = datetime.datetime.strptime(datespec[0], '%Y-%m-%d').date()
     except ValueError:
         try:
-            sleep_date = datetime.strptime(datespec[0], '%m-%d').date()
+            sleep_date = datetime.datetime.strptime(datespec[0], '%m-%d').date()
             sleep_date = sleep_date.replace(year=date.today().year)
         except ValueError:
             print('Invalid date argument. "', datespec[0], '"',
                   sep="", file=sys.stderr)
             sys.exit()
 
-    if (sleep_date > date.today()):
+    if (sleep_date > datetime.date.today()):
         print('Cannot enter future sleep (check your date argument)',
               file=sys.stderr)
         sys.exit()
@@ -43,20 +43,20 @@ def timespec_conv(timespec):
 
     times = timespec.split("-", 1)
     try:
-        start = datetime.strptime(times[0], '%H:%M').time()
+        start = datetime.datetime.strptime(times[0], '%H:%M').time()
     except ValueError:
         try:
-            start = datetime.strptime(times[0], '%H').time()
+            start = datetime.datetime.strptime(times[0], '%H').time()
         except ValueError:
             print('Bad time specification. "', timespec,
                   '"', sep="", file=sys.stderr)
             sys.exit()
 
     try:
-        end = datetime.strptime(times[1], '%H:%M').time()
+        end = datetime.datetime.strptime(times[1], '%H:%M').time()
     except ValueError:
         try:
-            end = datetime.strptime(times[1], '%H').time()
+            end = datetime.datetime.strptime(times[1], '%H').time()
         except ValueError:
             print('Bad time specification. "', timespec,
                   '"', sep="", file=sys.stderr)
@@ -114,8 +114,8 @@ def get_date_times(date):
     for row in db.execute("SELECT start_time, end_time " +
                           "FROM sleep_times WHERE date='"
                           + date.strftime("%Y-%m-%d") + "'"):
-        sleep_list.append((datetime.strptime(row[0], '%H:%M').time(),
-                           datetime.strptime(row[1], '%H:%M').time()))
+        sleep_list.append((datetime.datetime.strptime(row[0], '%H:%M').time(),
+                           datetime.datetime.strptime(row[1], '%H:%M').time()))
 
     return sleep_list
 
@@ -195,7 +195,7 @@ def scale_times(date, width):
         fraction_time = block * scale
         hours = math.floor(fraction_time)
         minutes = math.floor((fraction_time - hours) * 60)
-        block_time = time(hours, minutes)
+        block_time = datetime.time(hours, minutes)
         if(time_is_in_set(block_time, sleep_times)):
             if (time_is_in_set(block_time, ideal_time)):
                 first_half.append('#')
@@ -210,7 +210,7 @@ def scale_times(date, width):
         fraction_time = block * scale
         hours = math.floor(fraction_time)
         minutes = math.floor((fraction_time - hours) * 60)
-        block_time = time(hours, minutes)
+        block_time = datetime.time(hours, minutes)
         if(time_is_in_set(block_time, sleep_times)):
             if (time_is_in_set(block_time, ideal_time)):
                 second_half.append('#')
@@ -267,18 +267,18 @@ def draw_line(pos, date, win):
 def scroll_up(win, last_date):
     win.scroll(-1)
     height = win.getmaxyx()[0]-1
-    new_date = last_date - timedelta(days=height)
+    new_date = last_date - datetime.timedelta(days=height)
     draw_line(1, new_date, win)
-    return last_date - timedelta(days=1)
+    return last_date - datetime.timedelta(days=1)
 
 
 def scroll_down(win, last_date):
-    if (last_date == date.today()):
+    if (last_date == datetime.date.today()):
         return last_date
     else:
         win.scroll(1)
         height = win.getmaxyx()[0]-1
-        new_date = last_date + timedelta(days=1)
+        new_date = last_date + datetime.timedelta(days=1)
         draw_line(height, new_date, win)
         return new_date
 
@@ -286,7 +286,7 @@ def scroll_down(win, last_date):
 def page_up(win, last_date):
     win.scroll(-win.getmaxyx()[0])
     height = win.getmaxyx()[0]-1
-    last_date = last_date - timedelta(days=height)
+    last_date = last_date - datetime.timedelta(days=height)
     draw_screen(win, last_date)
     return last_date
 
@@ -294,7 +294,7 @@ def page_up(win, last_date):
 def page_down(win, last_date):
     win.scroll(win.getmaxyx()[0])
     height = win.getmaxyx()[0]-1
-    last_date = last_date + timedelta(days=height)
+    last_date = last_date + datetime.timedelta(days=height)
     if (last_date >= date.today()):
         last_date = date.today()
 
@@ -305,7 +305,7 @@ def page_down(win, last_date):
 def draw_screen(win, last_date):
     height = win.getmaxyx()[0]-1
     for pos in range(height, 0, -1):
-        draw_line(pos, last_date - timedelta(days=height-pos), win)
+        draw_line(pos, last_date - datetime.timedelta(days=height-pos), win)
     return last_date
 
 
@@ -358,7 +358,7 @@ def display_log():
         win = curses.newwin(screen.getmaxyx()[0], screen.getmaxyx()[1], 1, 0)
         win.keypad(1)
         win.scrollok(1)
-        last_date = date.today()
+        last_date = datetime.date.today()
         draw_screen(win, last_date)
 
         key = win.getch()
@@ -431,7 +431,7 @@ def main():
     if (args.date):
         sleep_date = datespec_conv(args.date)
     else:
-        sleep_date = date.today()
+        sleep_date = datetime.date.today()
 
     if (args.delete):
         delete_date_times(sleep_date)
